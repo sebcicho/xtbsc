aeed4e6dfd1b18e69bb157c28039331f4a4438fe
unknown <sebastian.cichocki1g@gmail.com>
2025-08-25 16:18:05 +0200

    better view

--- ui-xtbsc/src/components/currency-dashboard.tsx
+++ ui-xtbsc/src/components/currency-dashboard.tsx
@@ -56,7 +56,13 @@ export const CurrencyDashboard: React.FC<CurrencyDashboardProps> = ({limit}) => 
          <Card>
             <CardHeader>
                <div className='flex flex-col w-full'>
-                 <h2 className="text-2xl font-bold mb-8 text-foreground">Currencies Dashboard</h2>
+                <span className='flex mb-8'>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 mr-1">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 7.756a4.5 4.5 0 1 0 0 8.488M7.5 10.5h5.25m-5.25 3h5.25M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                  </svg>
+
+                  <h2 className="text-2xl font-bold text-foreground -mt-1">Currencies Dashboard</h2>
+                 </span>
                 <Input name='search' placeholder='Search 🔎︎' onChange={handleInputChange} ></Input>
               </div>
             </CardHeader>
--- ui-xtbsc/src/components/stock-dashboard.tsx
+++ ui-xtbsc/src/components/stock-dashboard.tsx
@@ -76,7 +76,17 @@ export const StockDashboard: React.FC<StockDashboardProps> = ({type, limit}) => 
          <Card>
             <CardHeader>
               <div className='flex flex-col w-full'>
-                <h2 className="text-2xl font-bold mb-8 text-foreground">{type === 'ETF' ? 'ETF' : 'Stock'} Dashboard</h2>
+                <span className='flex mb-8'>{type === 'ETF' ? 
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 mr-1">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z" />
+                  </svg>
+                  : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 mr-1">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
+                  </svg>
+                  } <h2 className="text-2xl font-bold  text-foreground -mt-1">{type === 'ETF' ? 'ETF' : 'Stock'} Dashboard</h2>
+                </span>
+                
                 <Input name='search' placeholder='Search 🔎︎' onChange={handleInputChange} ></Input>
               </div>
             </CardHeader>
